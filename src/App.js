d74f30673b4c7b27f7cdf1146046bdd850e0b9b2
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-05-25 20:25:50 +0200

    acced comments

--- src/App.js
+++ src/App.js
@@ -17,13 +17,17 @@ import SearchBar from './components/SearchBar';
 
 
 export default class App extends Component {
-
+    // Global state
     state = {
         searchedImages: [],
         isLoading: true,
         key: apiKey
     }
-
+    /*
+     Search functions gets the query which is later passed to the request string,
+     response is passed to the state, isLoading state is changed to false and query state is changed to search query
+     if fetching or parsing fails, the error is logged to the console
+    */
     searchFunction = (query = 'mountains') => {
         this.setState({isLoading:true, images:[]})
         axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this.state.key}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
@@ -39,6 +43,14 @@ export default class App extends Component {
             });
     }
 
+    /*
+     Main structure of the app
+     Container wraps the whole page
+     h1 title is rendered, the searchFunction is passed to the Nav and SearchBar components
+     Routing redirects "/" path to the starting url
+     Route for /search/:query renders the gallery and passes the props for the component
+     If no match is found the route is switched to render page not found component.
+    */
     render() {
         return (
             <Router>
@@ -50,7 +62,8 @@ export default class App extends Component {
                         <Route exact path="/" render={() => <Redirect to={`/search/mountains`}/>}/>
                         <Route exact path={'/search/:query'} render={({match}) =>
                             <Gallery
-                                routeMatch={match} data={this.state.searchedImages}
+                                routeMatch={match}
+                                data={this.state.searchedImages}
                                 queryData={this.state.query}
                                 handleSearch={this.searchFunction}
                                 loadingState={this.state.isLoading}
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,21 +4,33 @@ import React, {Component} from 'react';
 import GalleryItem from './GalleryItem';
 import NoResultsFound from './NoResultsFound';
 
+// Gallery class
+
 class Gallery extends Component {
 
+    // Running the search function passed from App.js for the first time
     componentDidMount() {
        this.props.handleSearch(this.props.routeMatch.params.query);
     }
 
+    /*
+     Checking if the URL matches the search phrase saved in props,
+     if no the search function in App.js runs
+    */
     componentDidUpdate(prevProps, prevState, snapshot) {
         if(this.props.routeMatch.params.query !== prevProps.routeMatch.params.query) {
             this.props.handleSearch(this.props.routeMatch.params.query)
         }
     }
 
+    /*
+     This code checks if the images matching the search phrase were found (array length)
+     if yes the data is passed to the GalleryItem.js which returns 24 individual photo objects to the pictures array
+     if no images were found the NoResultsFound component is rendered
+     during the loading of the results the Loading... message is rendered
+    */
     render() {
         const results = this.props.data;
-        console.log(results)
         let pictures;
 
 
